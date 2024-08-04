02f1d943de162a9e612650afad750a8b5ce7b557
ceciliapaiva <ceciliapaiva.pro@gmail.com>
2024-08-03 22:02:18 -0300

    Inicio

--- src/service/GeradorDeCod.java
+++ src/service/GeradorDeCod.java
@@ -9,9 +9,9 @@ public class GeradorDeCod implements Serializable {
     int codigo;
 
     public int gerarCod() {
-        for (int i = 0; i <= tamanhoCod; i++) {
+
             codigo = random.nextInt(10000);
-        }
+
         return codigo;
     }
 }
